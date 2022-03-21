e24a158337c968e6be3551653b2b4215ec916e8e
jjosegallegomesa <juan.gallegomesa@gmail.com>
2022-03-21 17:36:51 -0500

    ok

--- js/controlador.js
+++ js/controlador.js
@@ -1,17 +1,30 @@
 //IMPORTANDO MODULOS
 import {pintarTienda} from './tiendallenado.js'
-import{ampliarInformacion} from './ampliarInfo.js'
+
 
 //Llamando al modulo de pintar
 pintarTienda()
 
-//Llamando al modulo amplairInfo
+//Lanzando el modal
 let contenedorTienda=document.getElementById("fila")
-contenedorTienda.addEventListener("click",function(event){
-    
-    let modalinfoproducto = new bootstrap.Modal(document.getElementById('modalinfoproducto'))
-    ampliarInformacion(event)
-    modalinfoproducto.show()
 
+contenedorTienda.addEventListener("click",function(evento){
+
+    if(evento.target.classList.contains("btn")){
+       
+        let modalinfo = new bootstrap.Modal(document.getElementById('modalinformacion'))
+        
+        //cargar informaciond el producto dentro del modal
+        
+        let fotoInfo=document.getElementById("fotoInfo")
+        fotoInfo.src=evento.target.parentElement.querySelector("img").src
+
+        let tituloFoto=document.getElementById("nombreInfo")
+        tituloFoto.textContent=evento.target.parentElement.querySelector("h4").textContent
+        
+        modalinfo.show()
+
+
+    }
 
 })
