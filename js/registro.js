332456c1bbcedca7659f4d04ffdb44ca32617b74
test <test@test.com>
2022-04-27 08:20:00 -0500

    se crea modal registro

--- js/registro.js
+++ js/registro.js
@@ -1,6 +1,8 @@
 import { getAuth, createUserWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"
 
 let boton=document.getElementById("botonregistro")
+let modalregistro=new bootstrap.Modal(document.getElementById('mensajeinformativo'))
+let textoModal=document.getElementById("mensajemodalinfo")
 
 boton.addEventListener("click",function(evento){
 
@@ -14,6 +16,14 @@ boton.addEventListener("click",function(evento){
         // Signed in
         const user = userCredential.user;
         console.log("Exito en el registro")
+        textoModal.textContent="Exito en el registro"
+        modalregistro.show()
+        let formulario=document.getElementById("formularioregistro")
+        formulario.reset() 
+        setTimeout(function(){
+            modalregistro.hide()
+        },2000)
+
         // ...
     })
     .catch((error) => {
@@ -21,6 +31,11 @@ boton.addEventListener("click",function(evento){
         const errorMessage = error.message;
         // ..
         console.log("Fallo "+errorMessage)
+        textoModal.textContent="Fallamos: "+errorMessage
+        modalregistro.show()
+        setTimeout(function(){
+            modalregistro.hide()
+        },2000)
     });
 
 
